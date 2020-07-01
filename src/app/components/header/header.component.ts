650f6df1df3bf56ee7f12106693bc488e6ee17c0
Jimenez <jimenez.weerden@gmail.com>
2020-07-01 10:03:10 +0200

    feat: compact header  - sticky footer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { filter, takeUntil } from 'rxjs/operators';
+import { NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
   }
 
+  destroy$ = new Subject();
+  isHome: boolean;
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((routerEvent) => routerEvent instanceof NavigationEnd),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((routerEvent: RouterEvent) => this.isHome = routerEvent.url === '/');
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
