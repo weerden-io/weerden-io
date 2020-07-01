9e715df691e3bc078126e89073b041194777f5bb
Jimenez <jimenez.weerden@gmail.com>
2020-07-01 19:41:49 +0200

    fix: force ssl

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const enforce = require('express-sslify');
 
 const app = express();
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.static('./dist/weerden-io'));
 
 app.get('/*', (req, res) =>
