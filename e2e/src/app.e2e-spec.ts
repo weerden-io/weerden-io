ebbb75389622df0c644ad209b32f429e608fb1ca
Jimenez <jimenez.weerden@gmail.com>
2020-07-01 16:45:53 +0200

    refactor: rename to weerden-io

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('weerden-tv app is running!');
+    expect(page.getTitleText()).toEqual('weerden-io app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'weerden-tv'`, () => {
+  it(`should have as title 'weerden-io'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('weerden-tv');
+    expect(app.title).toEqual('weerden-io');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('weerden-tv app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('weerden-io app is running!');
   });
 });
