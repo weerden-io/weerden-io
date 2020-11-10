caca2b585c4ec07dc22df55db2ebc35b02526d0e
Jimenez <jimenez.weerden@gmail.com>
2020-11-10 23:33:25 +0100

    fix(api): force http to https redirect

--- server/config/routes.conf.ts
+++ server/config/routes.conf.ts
@@ -6,22 +6,17 @@ import * as enforce from 'express-sslify';
 export class RoutesConfig {
   static init(app: express.Application): void {
     app.use(bodyParser.json());
-    app.use(express.static(`${process.cwd()}/frontend/dist/weerden-io`));
-
     process.env.NODE_ENV === 'production'
       ? setupProd(app)
       : setupDev(app);
+
+    app.use(express.static(`${process.cwd()}/frontend/dist/weerden-io`));
   }
 }
 
 const setupProd = (app: express.Application) => {
   // Always enforce https on prod
   app.use(enforce.HTTPS({trustProtoHeader: true}));
-  app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    req.secure
-      ? next()
-      : res.redirect('https://' + req.headers.host + req.url);
-  });
 };
 
 const setupDev = (app: express.Application) => {
