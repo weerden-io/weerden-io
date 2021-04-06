27d23aa871d15ea1fed2c7ae571afb9dfbf307c4
Jimenez <jimenez.weerden@gmail.com>
2021-04-06 18:13:37 +0200

    chore(api): add generic async handler - use compression

--- server/api/rss/index.ts
+++ server/api/rss/index.ts
@@ -1,13 +1,9 @@
 import { Request, Response } from 'express';
 import * as Parser from 'rss-parser';
+import { catchAsync } from '../utils/catch-async';
 const parser = new Parser();
 
-export const getRss = async (req: Request, res: Response) => {
-  try {
+export const getRss = catchAsync(async (req: Request, res: Response) => {
     const rssFeed = await parser.parseURL('https://jimenezweerden.wordpress.com/feed/');
     res.send(rssFeed);
-  } catch {
-    res.status(500);
-    res.send('failed to fetch rss feed');
-  }
-};
+});
--- server/config/routes.conf.ts
+++ server/config/routes.conf.ts
@@ -2,10 +2,13 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as enforce from 'express-sslify';
+import * as compression from 'compression';
 
 export class RoutesConfig {
   static init(app: express.Application): void {
     app.use(bodyParser.json());
+    app.use(compression());
+
     process.env.NODE_ENV === 'production'
       ? setupProd(app)
       : setupDev(app);
