816bad6df7ce8e7951170a32f967b89aed47ab99
Jimenez <jimenez.weerden@gmail.com>
2021-04-21 21:16:17 +0200

    chore(api): update dependencies

---
+++ update-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+yarn
+git add .
+git commit -m "chore(api): update dependencies"
+
+npx yarn-audit-fix
+git add .
+git commit -m "chore(api): yarn audit fix"
+
+cd frontend && yarn
+git add .
+git commit -m "chore(frontend): update dependencies"
+
+npx yarn-audit-fix
+git add .
+git commit -m "chore(frontend): yarn audit fix"
