4b8ff9493c0ec952a6e565c4785cc5babc61e679
Jimenez <jimenez.weerden@gmail.com>
2021-06-20 22:40:59 +0200

    refactor(api): tweak ApiError code style

--- server/api/utils/ApiError.ts
+++ server/api/utils/ApiError.ts
@@ -1,16 +1,16 @@
 export class ApiError extends Error {
   statusCode: number;
   isOperational: boolean;
+  stack: string;
 
   constructor(statusCode: number, message: string, isOperational: boolean = true, stack: string = '') {
     super(message);
 
     this.statusCode = statusCode;
     this.isOperational = isOperational;
-    if (stack) {
-      this.stack = stack;
-    } else {
-      Error.captureStackTrace(this, this.constructor);
-    }
+
+    Boolean(stack)
+      ? this.stack = stack
+      : Error.captureStackTrace(this, this.constructor);
   }
 }
