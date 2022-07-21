dab73508db3413640657dabae6ab2f3112de2db2
Jimenez <jimenez.weerden@gmail.com>
2022-07-21 13:47:41 +0100

    chore: add audit-level to audit script

--- update-and-audit-dependencies.sh
+++ update-and-audit-dependencies.sh
@@ -3,7 +3,7 @@ yarn
 git add .
 git commit -m "chore(api): update dependencies"
 
-npx yarn-audit-fix@latest --force
+npx yarn-audit-fix@latest --force --audit-level high
 git add .
 git commit -m "chore(api): yarn audit fix"
 
@@ -11,6 +11,6 @@ cd ./frontend && yarn
 git add .
 git commit -m "chore(frontend): update dependencies"
 
-npx yarn-audit-fix@latest --force
+npx yarn-audit-fix@latest --force --audit-level high
 git add .
 git commit -m "chore(frontend): yarn audit fix"
