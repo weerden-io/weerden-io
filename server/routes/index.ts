24dbe3d458af2104092a945ec2110f328ad8e2ca
Jim <jimenez.weerden@gmail.com>
2025-12-19 12:18:50 +0100

    fix: server fixes (#60)

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,4 +1,4 @@
-import * as express from 'express';
+import express = require('express');
 import rateLimit from 'express-rate-limit';
 import { ApiRoutes } from '../api';
 
@@ -6,17 +6,15 @@ export class Routes {
   static init(app: express.Application, router: express.Router) {
     ApiRoutes.init(app);
 
-    router
-      .route('*')
-      .get((req: express.Request, res: express.Response) => {
-        res.sendFile('index.html', {root: `${process.cwd()}/frontend/dist/weerden-io`});
-      });
+    router.get(/.*/, (req, res) => {
+      res.sendFile('index.html', { root: `${process.cwd()}/frontend/dist/weerden-io` });
+    });
 
     const limiter = rateLimit({
-      windowMs: 60 * 1000, // 1 minute
-      max: 5
+      windowMs: 60 * 1000,
+      max: 5,
     });
-    // apply rate limiter to all requests
+
     app.use(limiter);
     app.use('/', router);
   }
